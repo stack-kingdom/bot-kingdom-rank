925d26ea58e7b4b97e41816157278d6806a10023
Gustavo Oly <gustavooly.dev@gmail.com>
2025-03-14 16:48:19 -0300

    feat: database
    Arquivo "data" com as configurações necessárias para criação do banco e sua tabela relacionada.

---
+++ data/database.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Arquivo responsável por criar o banco de dados e a tabela de usuários.
+ */
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+
+/**
+ * @function openDb
+ * @description Função para abrir o banco de dados
+ * @returns {Promise<sqlite3.Database>}
+ */
+async function openDb() {
+    return await open({
+        // atenção: este caminho é relativo ao arquivo que está executando o código, atualmente é src/main.js
+        filename: '../data/banco.db',
+        driver: sqlite3.Database,
+    });
+}
+
+/**
+ * @function createTable
+ * @description Função para criar a tabela de usuários
+ * @returns {Promise<void>}
+ */
+async function createTable() {
+    const db = await openDb();
+    db.run(
+        'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT, message_count INTEGER)'
+    );
+}
+
+export { openDb, createTable };
