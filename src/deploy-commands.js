9edc2fcc630a4f6cc39c33326b656f6e1e269b58
Gustavo Oly <gustavooly.dev@gmail.com>
2025-03-23 16:10:55 -0300

    feat: regras de pontuação
    arquivo com dados de pontuação e arquivo deploy de slash

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -50,13 +50,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 (async () => {
     try {
         console.log('Iniciando o deploy dos comandos...');
-        await rest.put(
-            Routes.applicationGuildCommands(
-                process.env.CLIENT_ID,
-                process.env.GUILD_ID
-            ),
-            { body: commands }
-        );
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands });
 
         console.log('Comandos deployados com sucesso!');
     } catch (error) {
