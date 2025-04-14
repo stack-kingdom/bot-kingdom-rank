a415bd026ed6b6e9edeed4d8ad2818f3e936501a
Gustavo Oly <gustavooly.dev@gmail.com>
2025-04-14 19:07:30 -0300

    refactor(rules): atualizar a formatação e o conteúdo das regras de pontuação

--- src/commands/utility/rules.js
+++ src/commands/utility/rules.js
@@ -23,19 +23,32 @@ const data = new SlashCommandBuilder()
 async function execute(interaction) {
     const embed = new EmbedBuilder()
         .setColor('#0099ff')
-        .setTitle('Regras de pontos: ')
-        .setImage('https://th.bing.com/th/id/OIG2.HFXTcK3gNTNKT.qmOXgl?w=1024&h=1024&rs=1&pid=ImgDetMain')
-        .addFields({
-            name: 'Mensagens:', value: `XP: ${rules.points.mensagens} por mensagem`, inline: false,
-        }, {
-            name: 'Atividade em call:',
-            value: `XP: ${rules.points.calls} à cada ${callsRounded} segundos`,
-            inline: false,
-        }, {
-            name: 'nitro', value: `XP: ${rules.points.nitro} por nitro enviado`, inline: false,
-        }, {
-            name: 'Spam não é permitido', value: 'Passível de punição.', inline: false,
-        })
+        .setTitle('Regras de pontos')
+        .setImage(
+            'https://i.pinimg.com/736x/b6/97/0b/b6970ba1e6d8773088b5dc4af68cb4f4.jpg'
+        )
+        .addFields(
+            {
+                name: 'Mensagens:',
+                value: `XP: ${rules.points.mensagens} por mensagem.`,
+                inline: false,
+            },
+            {
+                name: 'Atividade em call:',
+                value: `XP: ${rules.points.calls} à cada ${callsRounded} segundos.`,
+                inline: false,
+            },
+            {
+                name: 'Nitro:',
+                value: `XP: ${rules.points.nitro} por nitro enviado.`,
+                inline: false,
+            },
+            {
+                name: 'Spam:',
+                value: 'Passível de punição.',
+                inline: false,
+            }
+        )
         .setTimestamp()
         .setFooter({ text: `${rules.config.nome_do_bot}` });
 
